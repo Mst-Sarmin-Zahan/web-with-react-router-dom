4055fd11b3f2c6b6d21eab19ab337264928ceb37
Mst-Sarmin-Zahan <141745771+Mst-Sarmin-Zahan@users.noreply.github.com>
2023-11-09 00:14:32 +0600

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,12 +3,12 @@ const Menu =()=>{
     return(
         <div>
             <ul >
-                <li><Navlink className={(isActive)=>isActive?"active-item":"Pending-item"} to="/">Home</Navlink></li>
-                <li><Navlink className={(isActive)=>isActive?"active-item":"Pending-item"} to="/about">About</Navlink></li>
-                <li><Navlink className={(isActive)=>isActive?"active-item":"Pending-item"} to="/blog">Blog</Navlink></li>
-                <li><Navlink className={(isActive)=>isActive?"active-item":"Pending-item"} to="/project">Project</Navlink></li>
-                <li><Navlink className={(isActive)=>isActive?"active-item":"Pending-item"} to="/service">Service</Navlink></li>
-                <li><Navlink className={(isActive)=>isActive?"active-item":"Pending-item"} to="/contact">Contact</Navlink></li>
+                <li><NavLink className={(isActive)=>isActive?"active-item":"Pending-item"} to="/">Home</Navlink></li>
+                <li><NavLink className={(isActive)=>isActive?"active-item":"Pending-item"} to="/about">About</Navlink></li>
+                <li><NavLink className={(isActive)=>isActive?"active-item":"Pending-item"} to="/blog">Blog</Navlink></li>
+                <li><NavLink className={(isActive)=>isActive?"active-item":"Pending-item"} to="/project">Project</Navlink></li>
+                <li><NavLink className={(isActive)=>isActive?"active-item":"Pending-item"} to="/service">Service</Navlink></li>
+                <li><NavLink className={(isActive)=>isActive?"active-item":"Pending-item"} to="/contact">Contact</Navlink></li>
             </ul>
         </div>
     )
